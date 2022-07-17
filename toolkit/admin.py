1900d0edeef18258fbb80127de488d402cad3ec5
renatabiniek <renata.biniek@gmail.com>
2022-07-17 12:47:47 +0000

    Add functionality to manage comments in admin panel

--- toolkit/admin.py
+++ toolkit/admin.py
@@ -1,13 +1,20 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
-from .models import Tool
+from .models import Tool, Comment
 
 
 @admin.register(Tool)
 class ToolAdmin(SummernoteModelAdmin):
 
-    list_display = ['tool_name', 'author_name', 'published_status', 'date_added']
+    list_display = ('tool_name', 'author_name', 'published_status', 'date_added')
     search_fields = ['tool_name', 'method_details', 'keywords']
     prepopulated_fields = {'slug': ('tool_name',)}
     list_filter = ('date_added', 'published_status', 'author_name')
     summernote_fields = ('method_details')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    
+    list_display = ('tool', 'name', 'content', 'date_added', 'approved')
+    list_filter = ('name', 'date_added', 'approved')
+    search_fields = ['name', 'content']
