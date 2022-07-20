c40b2aaa4863ff79dfaae0f6691ece3b2ca6f4a4
renatabiniek <renata.biniek@gmail.com>
2022-07-20 22:40:01 +0000

    Create and link view for home page

--- toolkit/urls.py
+++ toolkit/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ToolList.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
+    path('toolkit/', views.ToolList.as_view(), name='tools'),
 ]
--- toolkit/views.py
+++ toolkit/views.py
@@ -5,5 +5,10 @@ from .models import Tool
 class ToolList(generic.ListView):
     model = Tool
     queryset = Tool.objects.filter(published_status=1).order_by('-date_added')
-    template_name = 'index.html'
+    template_name = 'tools.html'
     paginate_by = 8
+
+'''Renders a home page template'''
+
+class HomePageView(generic.TemplateView):
+    template_name = 'index.html'
