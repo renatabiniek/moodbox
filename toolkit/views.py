3923d56bf37b1f3b5021f817040fd32a8d8278fb
renatabiniek <renata.biniek@gmail.com>
2022-07-22 22:37:36 +0000

    Create view to display list of tools

--- toolkit/views.py
+++ toolkit/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Tool
 
+
 class ToolList(generic.ListView):
     model = Tool
     queryset = Tool.objects.filter(published_status=1).order_by('-date_added')
     template_name = 'tools.html'
-    paginate_by = 8
+    paginate_by = 6
 
 '''Renders a home page template'''
 
