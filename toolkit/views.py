98ad2d9a57d4e15c5c54599560549ba075a65718
renatabiniek <renata.biniek@gmail.com>
2022-07-24 17:30:27 +0000

    Create view for tool card detail page

--- toolkit/views.py
+++ toolkit/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Tool
 
 
@@ -8,6 +8,31 @@ class ToolList(generic.ListView):
     queryset = Tool.objects.filter(published_status=1).order_by('-date_added')
     template_name = 'tools.html'
     paginate_by = 6
+
+'''Displays detailed view of individual tool card'''
+class ToolDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Tool.objects.filter(published_status=1)
+        tool = get_object_or_404(queryset, slug=slug)
+        comments = tool.comments.filter(approved=True),order_by('date_added')
+        liked = False
+        if tool.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        
+        return render(
+            request,
+            'tool_detail.html',
+            {
+                'tool': tool,
+                'comments': comments,
+                'liked': liked
+            },
+        )
+
+
+
+
 
 '''Renders a home page template'''
 
